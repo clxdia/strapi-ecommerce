0cac111165c9517b43e6c22f77cdd205decfb640
blue cat <01claudia.parravano@gmail.com>
2023-03-16 18:13:22 +0100

    fix slider

--- src/ui/Swiper.js
+++ src/ui/Swiper.js
@@ -4,6 +4,10 @@ import React from "react";
 import { Navigation, Pagination, Autoplay } from "swiper";
 import Image from "next/image";
 
+import pic1 from "../images/slide1.png";
+import pic2 from "../images/slide2.png";
+import pic3 from "../images/slide3.png";
+
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 // Image by https://www.freepik.com/free-vector/flat-design-clothing-store-logo-design_28815161.htm#query=clothes%20store%20logo&position=38&from_view=search&track=sph
@@ -36,28 +40,13 @@ const SwiperComponent = () => {
           className={SwiperStyles.swiper}
         >
           <SwiperSlide className={SwiperStyles.swiper_slide}>
-            <Image
-              src="https://picsum.photos/3419/1195"
-              alt="/"
-              width="1000"
-              height="1000"
-            ></Image>
+            <Image src={pic1} alt="/" width="1000" height="1000"></Image>
           </SwiperSlide>
           <SwiperSlide className={SwiperStyles.swiper_slide}>
-            <Image
-              src="https://picsum.photos/3419/1195"
-              alt="/"
-              width="1000"
-              height="1000"
-            ></Image>
+            <Image src={pic2} alt="/" width="1000" height="1000"></Image>
           </SwiperSlide>
           <SwiperSlide className={SwiperStyles.swiper_slide}>
-            <Image
-              src="https://picsum.photos/3419/1195"
-              alt="/"
-              width="1000"
-              height="1000"
-            ></Image>
+            <Image src={pic3} alt="/" width="1000" height="1000"></Image>
           </SwiperSlide>
         </Swiper>
       </div>
