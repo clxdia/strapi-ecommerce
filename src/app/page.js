64004dc0b69569ea4ee2ef2adce66faa8aae5cf8
blue cat <01claudia.parravano@gmail.com>
2023-03-16 18:55:23 +0100

    fix ??

--- src/app/page.js
+++ src/app/page.js
@@ -16,11 +16,8 @@ export default function Home() {
       <main>
         <SwiperComponent />
         <SwiperMB />
-
         <NewArrivals />
-
         {/* <DealOfTheDay /> */}
-
         <Categories />
         {/* <Banner /> */}
         <Articles />
