04366901f10e479e4aeb92bb257c697db99f4b19
blue cat <01claudia.parravano@gmail.com>
2023-03-29 15:05:41 +0200

    add favicon

--- src/app/head.js
+++ src/app/head.js
@@ -3,6 +3,7 @@ export default function Head() {
     <>
       <title>E-Commerce Store</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" type="image/x-icon" href="/icon.ico"></link>
     </>
   );
 }
