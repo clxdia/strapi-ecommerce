6c86fa9be9196e2479951ae23bdbe193b8d2b25d
blue cat <01claudia.parravano@gmail.com>
2023-04-06 18:38:10 +0200

    try #3

--- src/app/page.js
+++ src/app/page.js
@@ -8,6 +8,7 @@ import SwiperComponent from "../ui/Swiper";
 import Categories from "../ui/Categories";
 import Articles from "../ui/Articles";
 import NewArrivals from "../ui/NewArrivals";
+import New from "../ui/New";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <Deals />
       <Categories />
       <Articles />
+      <New />
     </main>
   );
 }
