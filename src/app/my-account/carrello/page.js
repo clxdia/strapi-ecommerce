cc1450f36e3423d8ff9f751bafb01f78b85e3af4
blue cat <01claudia.parravano@gmail.com>
2023-04-07 18:23:45 +0200

    fix margins in cart for bigger screens

--- src/app/my-account/carrello/page.js
+++ src/app/my-account/carrello/page.js
@@ -39,7 +39,7 @@ const Carrello = () => {
   }
   return (
     <main className="md:w-[95%] w-[100%] mx-auto flex h-auto justify-between pb-[5rem] md:pb-[15rem] font-clash">
-      <div className="m-auto">
+      <div className="md:m-0 md:w-full m-auto">
         <h1 className="md:text-[2rem] text-center font-medium md:text-left text-[20px] mb-5">
           Il mio carrello
         </h1>
