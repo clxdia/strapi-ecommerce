da6cae77c461f2c64f35abf9e887c993dacde174
blue cat <01claudia.parravano@gmail.com>
2023-04-07 17:30:07 +0200

    add swiper

--- src/ui/Swiper.js
+++ src/ui/Swiper.js
@@ -3,59 +3,47 @@
 import { React, useEffect, useState } from "react";
 import Image from "next/image";
 
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import "../styles/swiper.css";
+
 import pic1 from "../images/desktop/slide1.png";
 import pic2 from "../images/desktop/slide2.png";
 import pic3 from "../images/desktop/slide3.png";
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
 
 const SwiperComponent = () => {
   const sliders = [pic1, pic2, pic3];
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [stroke, setStroke] = useState("#f07167");
-
-  const prevSlide = () => {
-    setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
-  };
-
-  const nextSlide = () => {
-    setCurrentSlide(currentSlide === 2 ? 0 : (prev) => prev + 1);
-  };
-  useEffect(() => {
-    if (currentSlide === 2) {
-      setStroke("#fdfcdc");
-    } else {
-      setStroke("#f07167");
-    }
-  });
-
   return (
-    <div className="md:block sm:block hidden h-auto overflow-x-hidden w-[100%] relative">
-      <div
-        className="w-[300vw] flex h-fit -translate-x-[100vw] transition-all ease-out duration-[1s]"
-        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+    <>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
       >
-        <Image src={sliders[0]} alt="" className="w-[100%] h-full" />
-        <Image src={sliders[1]} alt="" className="w-[100%] h-[100%]" />
-        <Image src={sliders[2]} alt="" className="w-[100%] h-[100%]" />
-      </div>
-
-      <div className="absolute md:left-10 left-6 top-1/2 p-4 cursor-pointer">
-        <BsChevronLeft
-          onClick={prevSlide}
-          size={40}
-          style={{ stroke: stroke }}
-          className={"stroke-[1px] stroke-red-600 cursor-pointer "}
-        />
-      </div>
-      <div className="absolute md:right-10 right-6 top-1/2 p-4">
-        <BsChevronRight
-          onClick={nextSlide}
-          size={40}
-          style={{ stroke: stroke }}
-          className={"stroke-[1px] cursor-pointer"}
-        />
-      </div>
-    </div>
+        <SwiperSlide>
+          <Image src={pic1} alt="/" width="1000" height="1000"></Image>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={pic2} alt="/" width="1000" height="1000"></Image>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={pic3} alt="/" width="1000" height="1000"></Image>
+        </SwiperSlide>
+      </Swiper>
+    </>
   );
 };
 
