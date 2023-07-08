eb9f78ca9b69be8e59f29293160be963f582cfe4
blue cat <01claudia.parravano@gmail.com>
2023-07-08 18:29:12 +0200

    add wake up server function

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
+"use client";
 import React from "react";
 
 import "../styles/globals.css";
+import { useEffect } from "react";
 
 import Deals from "../ui/Deals";
 import SwiperMB from "../ui/SwiperMB";
@@ -10,6 +12,23 @@ import Articles from "../ui/Articles";
 import NewArrivals from "../ui/NewArrivals";
 
 export default function Home() {
+  useEffect(() => {
+    const wakeUpServer = async () => {
+      try {
+        await fetch(process.env.NEXT_PUBLIC_SERVER);
+        console.log("Server woke up!");
+      } catch (error) {
+        console.error("Error waking up server:", error);
+      }
+    };
+
+    wakeUpServer();
+    const interval = setInterval(wakeUpServer, 900000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
   return (
     <div>
       <div className="md:block sm:block hidden">
