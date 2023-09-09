0e23be8dc9727d6961cfd71f1929bde418178029
blue cat <01claudia.parravano@gmail.com>
2023-09-09 17:44:24 +0200

    fix ui in Deals

--- src/ui/Deals.js
+++ src/ui/Deals.js
@@ -42,7 +42,7 @@ export default async function Deals() {
             </button>
           </div>
 
-          <div className="min-w-[60%] flex overflow-y-hidden overflow-x-scroll gap-5">
+          <div className="flex overflow-y-hidden overflow-x-scroll gap-5">
             {items.data.map((item) => (
               <div
                 className="min-w-[220px] h-[350px] md:min-w-[280px] md:min-h-full"
