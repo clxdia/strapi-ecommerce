6fc874fafd22a260fdd3fdd953485aeaf67fc149
clxdia <claudiaparravano2001@gmail.com>
2024-03-09 16:31:58 +0100

    fixed ui and link bugs in emptypage.js

--- src/ui/EmptyPage.js
+++ src/ui/EmptyPage.js
@@ -13,8 +13,8 @@ const EmptyPage = () => {
         <p className="text-[13px] md:text-[17px] w-[90%] m-auto">
           Trovi qui le info sui tuoi aggiornamenti, resi o rimborsi.
         </p>
-        <button className="mt-5 mb-4 flex bg-blueish text-buttontext p-3 w-[80%] gap-3 justify-center items-center border-[2px] border-blueish hover:ease-in transition ease-in-out delay-150 hover:shadow-headline/50 hover:shadow-xl rounded-xl font-satoshi font-bold uppercase tracking-wide">
-          <Link href="./view-all" className="hover:cursor-pointer">
+        <button className="mt-5 mb-4 flex bg-blueish text-buttontext p-3 w-[80%] gap-3 justify-center items-center border-[2px] border-blueish hover:ease-in transition ease-in-out delay-150 hover:shadow-headline/50 hover:shadow-xl rounded-xl font-satoshi font-bold uppercase tracking-wide mx-auto">
+          <Link href="/view-all" className="hover:cursor-pointer">
             Continua lo shopping
           </Link>
         </button>
